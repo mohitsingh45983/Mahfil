4db38225f392ce51daef74f29e26a4c7393e73d2
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-23 16:05:53 +0530

    Update profile photo route,cloudinary initialize and protectRoute done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -82,3 +82,28 @@ export const logout = (req, res) => {
     return res.status(500).json({ message: 'Internal Server Error' })
   }
 }
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body
+    const userId = req.user._id
+
+    if (!profilePic) {
+      return res.status(400).json({ message: 'Profile pic is required' })
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic)
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    )
+
+    res.status(200).json(updatedUser)
+  } catch (err) {
+    console.log('Error in login controller', err.message)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js'
 
 dotenv.config() // to use env file
 const app = express()
 
-app.use(express.json());  // extract json data from body
+app.use(express.json()) // extract json data from body
+app.use(cookieParser())
+
 app.use('/api/auth', authRoutes)
 
 const port = process.env.PORT
