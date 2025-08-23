3494ccc9c1d2fbcf586486b07dffd8922b66d28f
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-23 11:22:53 +0530

    signUp route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,43 @@
-export const login = (req,res) => {
-    res.send("login route");
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body()
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: 'Password must be at least 6 characters' })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).json({ message: 'Email already exists' })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({ email, fullName, password: hashedPassword })
+
+    if(newUser){
+         return res
+        .status(400)
+        .json({ message: 'Invalid User Data'});
+    }
+    else{
+        //generate jwt token
+
+       
+    }
+  } catch (err) {}
 }
 
-export const signup = (req,res) => {
-    res.send("signup route");
+export const login = (req, res) => {
+  res.send('login route')
 }
 
-export const logout = (req,res) => {
-    res.send("logout route");
+export const logout = (req, res) => {
+  res.send('logout route')
 }
