bb20dd56c4f73be0c800c82506f915a38ffef436
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-23 14:58:30 +0530

    logout route done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,5 +74,11 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-  res.send('logout route')
+  try {
+    res.cookie('jwt', '', { maxAge: 0 })
+    return res.status(200).json({ message: 'Logged out successfully' })
+  } catch {
+    console.log('Error in login controller', err.message)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
 }
