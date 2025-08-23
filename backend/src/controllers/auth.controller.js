02c8e3e9f8228c18de4d7e041a3dd3eabde498a4
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-23 12:44:45 +0530

    signUp route done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,13 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+import { generateToken } from '../lib/utils.js'
 
 export const signup = async (req, res) => {
-  const { fullName, email, password } = req.body()
+  const { fullName, email, password } = req.body
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' })
+    }
     if (password.length < 6) {
       return res
         .status(400)
@@ -21,17 +25,25 @@ export const signup = async (req, res) => {
 
     const newUser = new User({ email, fullName, password: hashedPassword })
 
-    if(newUser){
-         return res
-        .status(400)
-        .json({ message: 'Invalid User Data'});
-    }
-    else{
-        //generate jwt token
+    if (newUser) {
+      //generate jwt token
+      generateToken(newUser._id, res)
+      await newUser.save()
 
-       
+      //201 means something has been created
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      })
+    } else {
+      return res.status(400).json({ message: 'Invalid User Data' })
     }
-  } catch (err) {}
+  } catch (err) {
+    console.log('Error in SignUp controller', err.message)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
 }
 
 export const login = (req, res) => {
