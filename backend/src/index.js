8be2e5beabe50bff37a842a2bb491b214e15a9a6
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-24 17:59:28 +0530

    all routes have been protected

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import { connectDB } from './lib/db.js'
 
 dotenv.config() // to use env file
@@ -10,6 +11,11 @@ const app = express()
 
 app.use(express.json()) // extract json data from body
 app.use(cookieParser())
+
+app.use(cors({
+  origin: "http://localhost:5173",  // your frontend URL
+  credentials: true                // if using cookies/auth
+}));
 
 app.use('/api/auth', authRoutes)
 
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,26 @@
+// store.js
+import { create } from 'zustand'
+import {axiosInstance} from "../lib/axios.js"
+
+export const useAuthStore = create((set) => ({
+    authUser : null,
+    isSigningUp:false,
+    isLoggingIng:false,
+    isUpdatingProfile:false,
+    isCheckingAuth: true,
+
+    checkAuth:async() =>{
+        try{
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser:res.data});
+        }
+        catch(err){
+            console.log("Error in checkAuth",err);
+            set({authUser:null});
+        }
+        finally{
+            set({isCheckingAuth:false});
+        }
+    }
+}))
+
