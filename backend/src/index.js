ddc86a68352cf3496a5e8c074131d538b93f72a5
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-24 11:31:35 +0530

    Message  routes done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js'
 
@@ -11,6 +12,8 @@ app.use(express.json()) // extract json data from body
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
+
+app.use('/api/message', messageRoutes)
 
 const port = process.env.PORT
 
