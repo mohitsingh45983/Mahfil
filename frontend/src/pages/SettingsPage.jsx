33f3b1e79052dbf8dc959ec12610ad455268c220
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-25 23:41:32 +0530

    settings page update

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
 const SettingsPage = () => {
   return (
-    <div>
-      
+    <div className="h-screen flex flex-col items-center justify-center bg-base-100">
+      <img
+        src="/undraw_maintenance_rjtm.svg"
+        alt="Logo"
+        className="w-80 h-80 mb-8"
+      />
+      <h1 className="text-3xl font-bold text-primary mb-2">
+        Under Maintanence
+      </h1>
+      <p className="text-base-content/70">This page is coming soon.</p>
     </div>
   )
 }
