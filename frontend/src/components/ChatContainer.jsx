50a21bc47d189236028c88428905ceb51aa8863c
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-26 22:53:00 +0530

    chat header,Input message done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useChatStore } from '../store/useChatStore'
+import { useEffect, useRef } from 'react'
+
+import ChatHeader from './ChatHeader'
+import MessageInput from './MessageInput'
+import MessageSkeleton from './skeletons/MessageSkeleton'
 
 const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore()
+
+  useEffect(() => {
+    getMessages(selectedUser._id)
+  }, [selectedUser._id, getMessages])
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+      </div>
+    )
+  }
+
   return (
-    <div>
-      
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <MessageInput/>
     </div>
   )
 }
-
 export default ChatContainer
