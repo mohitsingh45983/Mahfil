e3550719a52f95c35941649ad8558603d0ce10ae
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-27 23:40:43 +0530

    working on sockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,9 @@ import messageRoutes from './routes/message.route.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { connectDB } from './lib/db.js'
+import {io, server, app} from "./lib/socket.js"
 
 dotenv.config() // to use env file
-const app = express()
 
 app.use(express.json({ limit: '10mb' })) // or higher if needed
 app.use(cookieParser())
@@ -25,7 +25,7 @@ app.use('/api/messages', messageRoutes)
 
 const port = process.env.PORT
 
-app.listen(port, (req, res) => {
+server.listen(port, (req, res) => {
   console.log(`Server is running on port ${port}`)
   connectDB()
 })
