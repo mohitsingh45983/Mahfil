1ea1471ba3dbe3cf3a3b0190fb3fcfea54096531
mohitsingh45983 <148526559+mohitsingh45983@users.noreply.github.com>
2025-08-28 18:39:55 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   const distPath = path.join(__dirname, '../../frontend/dist')
   app.use(express.static(distPath))
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'))
   })
 }
